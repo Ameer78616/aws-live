10db934f7ba9c6ff207e32810ed2eaf3c9b09662
Ameer78616 <79590635+Ameer78616@users.noreply.github.com>
2022-09-22 04:17:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "emloyee.c3uzcecwjmm7.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "addemployeeee"
+customregion = "ap-south-1"
